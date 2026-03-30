b440e539dfd044704854ddc0f37416cdc2d04175
Ali Can Yücel <yucelalican@hotmail.com>
2026-03-30 20:44:37 +0300

    Add project files.

---
+++ ACY.SmartEnums/Class1.cs
@@ -0,0 +1,34 @@
+﻿namespace ACY.SmartEnum;
+
+public abstract class SmartEnum<TEnum> where TEnum : SmartEnum<TEnum>
+{
+    public string Name { get; }
+    public int Value { get; }
+
+    protected SmartEnum(string name, int value)
+    {
+        Name = name;
+        Value = value;
+        _items.Add((TEnum)this);
+    }
+
+    private static readonly List<TEnum> _items = new List<TEnum>();
+
+    public static IReadOnlyCollection<TEnum> List => _items.AsReadOnly();
+
+    public static TEnum FromName(string name)
+    {
+        var match = _items.FirstOrDefault(x => x.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+        if (match == null) throw new ArgumentException($"No SmartEnum with name '{name}' found.");
+        return match;
+    }
+
+    public static TEnum FromValue(int value)
+    {
+        var match = _items.FirstOrDefault(x => x.Value == value);
+        if (match == null) throw new ArgumentException($"No SmartEnum with value '{value}' found.");
+        return match;
+    }
+
+    public override string ToString() => Name;
+}
